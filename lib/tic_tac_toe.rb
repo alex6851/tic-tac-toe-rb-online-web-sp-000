106fef83057e14498ec6f4822a098393d887e2f5
Alexander Baker <alex.baker685@gmail.com>
2018-11-01 21:28:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
 
-    move(board, index, current_player(board))
+    move(board, index, token)
     display_board(board)
   else turn(board)
   end
